fe432770830c962a72297d2a762d10958a6354ce
N-Hidaya <nurul-h@hotmail.com>
2024-11-19 00:27:08 +0800

    added stripe scripts

--- payment_app/payments/views.py
+++ payment_app/payments/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Transaction
+from django.conf import settings
+import stripe
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def initiate_payment(request):
+    if request.method == 'POST':
+        amount = int(request.POST.get('amount')) * 100 #Amount in cents
+        #Logic to create a transaction entry
+        #transaction = Transaction.objects.create(user=request.user, amount=amount, status='pending')
+
+        #Integrate with Stripe here
+        intent = stripe.PaymentIntent.create(
+            amount=amount,
+            currency='sgd',
+            payment_method_types=['card']
+        )
+        #Pass client secret to frontend
+        return render(request, 'payments/initiate_payment.html', {'client_secret': intent.client_secret})
+
+        #return HttpResponse("Initiating payment...")
+    
+    return render(request, 'payments/initiate_payment.html')
